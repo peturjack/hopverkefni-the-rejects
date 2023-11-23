d0317594ccd2f95501a575ab8699fe80481aaa4f
solal <solald@gmail.com>
2023-11-23 16:03:27 +0000

    created pages for navbar folder (burger and menu)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ButtonPrimary, AppLoginButton } from './components/Button';
 import { GlobalStyles } from './styles/Global';
+import Navbar from './components/navbar/Navbar';
 
 
  function App() {
   return (
     <>
+    <Navbar>NAV</Navbar>
     
     
     </>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Navbar = styled.div`
+    position: absolute;
+    top: 5%;
+    right: 0;
+    width: 3rem;
+    height: 3rem;
+    margin: 20px;
+    color: rgb(134, 35, 98);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    border: none;
+    padding: 0;
+    `
+
+export default Navbar

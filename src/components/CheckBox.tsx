8dec873b7915925c7f7e2e4b3551bfc1049f9f44
solal <solald@gmail.com>
2023-12-02 16:15:18 +0000

    trying to fix checkbox border

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
 import styled from 'styled-components';
+
+
 type Props={ 
-text: string; 
-}
-const CheckboxContainer = styled.label`
+  text: string; 
+  }
+  
+const CheckboxContainer = styled.label`  
   display: flex;
   align-items: Center;
-  padding-top:0.8rem;
-
+  padding-top: 0.8rem;
 
 `;
+
 const CheckboxInput = styled.input`
   margin-right:10px;
-border:none;
 `;
+
 const CheckboxText = styled.p`
   margin: 0;
- 
   `;
+
 const CheckBox = ({ text }:Props) => (
   <CheckboxContainer>
-    <CheckboxInput type="checkbox" />
+    <CheckboxInput type="checkbox"/>
     <CheckboxText>{text}</CheckboxText>
   </CheckboxContainer>
 );
